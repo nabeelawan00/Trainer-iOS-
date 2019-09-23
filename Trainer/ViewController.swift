0aa2e0de3951684980ac066e8fdfeafe8b9a70a5
Bills Mac <nabeelawan.g@gmail.com>
2019-09-23 17:40:25 +0500

    Initial Commit

---
+++ Trainer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trainer
+//
+//  Created by Bills Mac on 23/09/2019.
+//  Copyright © 2019 Bills Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
